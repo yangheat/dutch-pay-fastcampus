8fe98b9d6110a3caab0bbf4c66386bd99e419716
yangheat <yangheat@gamil.com>
2023-01-08 22:38:00 +0900

    add members test code

---
+++ src/components/AddMembers.spec.jsx
@@ -0,0 +1,44 @@
+import userEvent from "@testing-library/user-event"
+import { RecoilRoot } from "recoil"
+import { AddMembers } from "./AddMembers"
+
+const renderComponent = () => {
+    render(
+        <RecoilRoot>
+            <AddMembers/>
+        </RecoilRoot>
+    )
+
+    // <div data-testid="id"></div>
+    const input = screen.getByTestId('input-members')
+    const saveBtn = screen.getByText('저장')
+
+    return {input, saveBtn}
+}
+
+
+describe('멤버 추가 페이지', () => {
+    test('멤버 추가 컴포넌트 랜더링', () => {
+        const {input, saveBtn} = renderComponent()
+
+        input.not.toBeNull()
+        saveBtn.not.toBeNull()
+    })
+    test('멤버를 추가하지 않고 저장 버튼 클릭 시, 에러 메시지 출력', async () => {
+        const {saveBtn} = renderComponent()
+
+        await userEvent.click(saveBtn)
+        
+        const errMsg = await screen.findByText('그룹 멤버들의 이름을 입력해주세요.')
+        expect(errMsg).toBeInTheDocument()
+    })
+    test('멤버 추가 후 저장 버튼 클릭 시, 저장 성공', async () => {
+        const {input, saveBtn} = renderComponent()
+
+        await userEvent.type(input, '철수 영희 영수')
+        await userEvent.click(saveBtn)
+        
+        const errMsg = screen.queryByText('그룹 멤버들의 이름을 입력해주세요.')
+        expect(errMsg).toBeNull()
+    })
+})
