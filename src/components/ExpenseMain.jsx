22034d087316b2bdd2756f9d4d65d40efe62e78a
yangheat <yangheat@gamil.com>
2023-02-10 00:24:19 +0900

    expense list page styling - seperate part 'add expense form' and part 'expense table'

--- src/components/ExpenseMain.jsx
+++ src/components/ExpenseMain.jsx
@@ -1,17 +1,56 @@
-import { Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
+import { useRecoilValue } from "recoil"
+import styled from "styled-components"
+import { groupNameState } from "../state/groupName"
 import { AddExpenseForm } from "./AddExpenseForm"
 import ExpenseTable from "./ExpenseTable"
 
 export const ExpenseMain = () => {
     return (
-        <div>
-            ExpenseMain Component
-            <div>
-                <AddExpenseForm />
-            </div>
-            <div>
-                <ExpenseTable />
-            </div>
-        </div>
+        <Container fluid>
+            <Row>
+                <Col xs={12} sm={5} md={4}>
+                    <LeftPane />
+                </Col>
+                <Col>
+                    <RightPane />
+                </Col>
+            </Row>
+            
+            
+        </Container>
     )
 }
+
+
+const LeftPane = () => (
+    <Container>
+        <AddExpenseForm />
+    </Container>
+)
+
+const RightPane = () => {
+    const groupName = useRecoilValue(groupNameState)
+    return (
+        <StyledContainer>
+            <Row>
+                <StyledGroupName>{groupName || '그룹 이름'}</StyledGroupName>
+            </Row>
+            <Row>
+                <ExpenseTable />
+            </Row>
+        </StyledContainer>
+    )
+}
+
+const StyledContainer = styled(Container)`
+    padding: 100px 31px 100px 31px;
+`
+
+const StyledGroupName = styled.h2`
+    text-align: center;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 48px;
+    margin-bottom: 80px;
+`
