1fbf788686940287f676607f384a23b01769a75b
yangheat <yangheat@gamil.com>
2023-02-10 00:14:42 +0900

    정산 리스트 테스트 코드

--- src/components/ExpenseMain.spec.jsx
+++ src/components/ExpenseMain.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { RecoilRoot } from "recoil"
 import { groupMembersState } from "../state/groupMembers"
@@ -31,17 +31,17 @@ const renderComponent = () => {
 }
 
 describe('비용 정산 메인 페이지', () => {
-    test('비용 추가 컴포넌트 렌더링', () => {
-        const {dateInput, descriptionInput, costInput, payerInput, addBtn} = renderComponent()
-        
-        expect(dateInput).toBeInTheDocument()
-        expect(descriptionInput).toBeInTheDocument()
-        expect(costInput).toBeInTheDocument()
-        expect(payerInput).toBeInTheDocument()
-        expect(addBtn).toBeInTheDocument()
-    })
-
     describe('비용 추가 컴포넌트', () => {
+        test('비용 추가 컴포넌트 렌더링', () => {
+            const {dateInput, descriptionInput, costInput, payerInput, addBtn} = renderComponent()
+            
+            expect(dateInput).toBeInTheDocument()
+            expect(descriptionInput).toBeInTheDocument()
+            expect(costInput).toBeInTheDocument()
+            expect(payerInput).toBeInTheDocument()
+            expect(addBtn).toBeInTheDocument()
+        })
+
         test('필수 항목을 입력하지 않고 "추가" 버튼 클릭 시, 오류 메시지 출력', async () => {
             const {addBtn, descriptionErrMsg, costErrMsg, payerErrMsg} = renderComponent()
             
@@ -71,26 +71,46 @@ describe('비용 정산 메인 페이지', () => {
             expect(payerErrMsg).toHaveAttribute('data-valid', 'true')
         })
     })
+    
 
+    describe('비용 리스트 컴포넌트', () => {
+        test('비용 리스트 컴포넌트 렌더링', () => {
+            renderComponent()
+            const expenseListComponent = screen.getByTestId('expenseList')
 
-    // test('비용에 대한 설명을 입력하지 않고 추가 버튼 클릭 시, 에러 메시지 출력', () => {
-    //     const {descriptionInput, addBtn} = renderComponent()
+            expect(expenseListComponent).toBeInTheDocument()
+        })
 
-    // })
+        test('날짜, 내용, 결제자, 금액 데이터를 입력 후 추가 버튼 클릭 시 정산 리스트에 출력', async () => {
+            const {dateInput, descriptionInput, costInput, payerInput, addBtn} = renderComponent()
 
-    // test('비용을 입력하지 않고 추가 버튼 클릭 시, 에러 메시지 출력', () => {
-    //     const {costInput, addBtn} = renderComponent()
-    // })
+            await Promise.all([
+                userEvent.type(dateInput, '2023-02-07'),
+                userEvent.type(descriptionInput, '장보기'),
+                userEvent.type(costInput, '30000'),
+                userEvent.selectOptions(payerInput, '영수')
+            ])
 
-    // test('결제자 입력하지 않고 추가 버튼 클릭 시, 에러 메시지 출력', () => {
-    //     const {payerInput, addBtn} = renderComponent()
-    // })
+            await userEvent.click(addBtn)
 
-    // test('정산 결과 렌더링', ()=> {
+            const expenseListComponent = screen.getByTestId('expenseList')
+            
+            const dateValue = within(expenseListComponent).getByText('2023-02-07')
+            expect(dateValue).toBeInTheDocument()
 
-    // })
+            const descriptionValue = within(expenseListComponent).getByText('장보기')
+            expect(descriptionValue).toBeInTheDocument()
 
-    // test('비용 리스트 렌더링', () => [
+            const costValue = within(expenseListComponent).getByText('영수')
+            expect(costValue).toBeInTheDocument()
 
-    // ])
+            const payerValue = within(expenseListComponent).getByText('30000 원')
+            expect(payerValue).toBeInTheDocument()
+
+            const amountValue = within(expenseListComponent).getByText('2023-02-07')
+            expect(amountValue).toBeInTheDocument()
+        })
+
+        
+    })
 })
